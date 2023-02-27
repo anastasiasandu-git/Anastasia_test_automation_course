fb47b77bb7cb52d99834ec5e9e7c9f01db250ceb
anastasiasandu-git <agaiducov@gmail.com>
2023-02-27 22:55:22 +0200

    Modification Commit

---
+++ topic2/src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    protected String prenume = "Sandu";
+    protected int Marinel;
+    private String gen = "Masculin";
+    public String nume = "Marinel";
+
+    void method() {
+        System.out.println(nume);
+
+    }
+}
---
+++ topic2/src/main.java
@@ -0,0 +1,17 @@
+public class main extends Persona {
+
+    public String Persona = "Marinel";
+
+    public void Persona (int Marinel) {
+        this.Marinel = Marinel;
+        super.Marinel = Integer.parseInt(String.valueOf(nume));
+    }
+
+    void method() {
+        System.out.println("nume");
+
+    }
+
+
+
+}
